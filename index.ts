14de843bed45b65f049212aa7984643f0653ba3f
David Morcillo Muñoz <david.morcillo@gmail.com>
2017-03-03 21:04:32 +0100

    Fix npm package

--- index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/board';
-export * from './lib/game';
-export * from './lib/action';
-export * from './lib/piece';
-export * from './lib/tak';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './board';
+export * from './game';
+export * from './action';
+export * from './piece';
+export * from './tak';
